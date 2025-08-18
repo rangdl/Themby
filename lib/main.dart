88bfc84387673c24a6ad6ad0ee9c762199709ed0
rang <378694192@qq.com>
2025-08-18 17:03:29 +0800

    111

--- lib/main.dart
+++ lib/main.dart
@@ -14,33 +14,25 @@ import 'package:themby/src/helper/prefs_provider.dart';
 import 'package:path/path.dart';
 import 'objectbox.g.dart';
 
-
-
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
 
   // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   final prefs = await SharedPreferences.getInstance();
-  final directory = await getApplicationDocumentsDirectory();
-  final store = Store(getObjectBoxModel(), directory: join(directory.path, 'objectbox'));
   final deviceInfo = await initDevice();
   PackageInfo packageInfo = await PackageInfo.fromPlatform();
+  final directory = await getApplicationDocumentsDirectory();
+  final store = Store(getObjectBoxModel(), directory: join(directory.path, packageInfo.appName));
 
-  runApp(
-      ProviderScope(
-        overrides: [
-          sharedPreferencesProvider.overrideWithValue(prefs),
-          storeProvider.overrideWithValue(store),
-          deviceInfoProvider.overrideWithValue(deviceInfo),
-          packageInfoProvider.overrideWithValue(packageInfo)
-        ],
-        child: const App()
-      )
-  );
+  runApp(ProviderScope(overrides: [
+    sharedPreferencesProvider.overrideWithValue(prefs),
+    deviceInfoProvider.overrideWithValue(deviceInfo),
+    packageInfoProvider.overrideWithValue(packageInfo),
+    storeProvider.overrideWithValue(store),
+  ], child: const App()));
 
   // Timer(const Duration(milliseconds: 500), () {
   //   FlutterNativeSplash.remove();
   // });
 }
-
